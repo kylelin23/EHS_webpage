d2ed65423d5fb3e137bee74425ce473a1c210a97
Kyle Lin <linkyle0924@gmail.com>
2025-08-24 15:19:25 -0700

    Add parsed data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
+import Papa from 'papaparse';
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+      fetch('sampleData.csv')
+        .then(response => response.text())
+        .then(text => {
+          const result = Papa.parse(text, { header: true })
+          setData(result.data)
+        })
+    }, [])
 
   return (
     <div className = "container">
       <div className = "header">
         <h1>Teaching Practices</h1>
+      </div>
+      <div className = "dataContainer">
+        <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
     </div>
   )
