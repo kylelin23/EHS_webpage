b6fe594b681d6d62014cb5caaa6a595aea0802f8
Kyle Lin <linkyle0924@gmail.com>
2025-08-26 20:40:07 -0700

    Debug

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,36 @@ import { useEffect, useState } from 'react'
 
 function App() {
 
-  const [data, setData] = useState([])
 
+  const [data, setData] = useState([]);
+
+  // Grab data from sampleData.csv
   useEffect(() => {
       fetch('sampleData.csv')
         .then(response => response.text())
         .then(text => {
-          const result = Papa.parse(text, { header: true })
-          setData(result.data)
+          const result = Papa.parse(text, { header: true }) // Put parsed data in result
+          setData(result.data) // data = result
         })
     }, [])
 
+    // CODE PARSING HERE:
+    // Parsed data is in variable called data
+    
+    console.log(data[0]);
+
   return (
+
     <div className = "container">
       <div className = "header">
         <h1>Teaching Practices</h1>
       </div>
       <div className = "dataContainer">
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <pre className = "data">{JSON.stringify(data, null, 2)}</pre>
       </div>
     </div>
   )
 }
 
 export default App
+
