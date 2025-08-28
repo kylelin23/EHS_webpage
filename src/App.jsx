6ae1fd50ad0dd8084f566a8f4a8540db24a50b07
Kyle Lin <linkyle0924@gmail.com>
2025-08-28 15:35:54 -0700

    Update dropdowns

--- src/App.jsx
+++ src/App.jsx
@@ -1,142 +1,87 @@
 import './App.css'
 import Papa from 'papaparse';
 import { useEffect, useState } from 'react'
+import Select from 'react-select';
 
 function App() {
 
   // Initializing Variables:
   const [data, setData] = useState([]);
 
-  const [measure, setMeasure] = useState("");
-  const [measureOpen, setMeasureOpen] = useState(false);
-  const [ATLOpen, setATLOpen] = useState(false);
-  const [SEDOpen, setSEDOpen] = useState(false);
-  const [LLDOpen, setLLDOpen] = useState(false);
-  const [COGOpen, setCOGOpen] = useState(false);
-  const [PHYOpen, setPHYOpen] = useState(false);
+  const [selectedMeasure, setSelectedMeasure] = useState(null);
+  const [selectedDevelopmentalLevel, setSelectedDevelopmentalLevel] = useState(null);
+  const [selectedTeachingStrats, setSelectedTeachingStrats] = useState(null);
+  const [selectedResources, setSelectedResources] = useState(null);
 
-
-  const [developmentalLevel, setdevelopmentalLevel] = useState("");
-  const [developmentalLevelOpen, setDevelopmentalLevelOpen] = useState(false);
-
-  const [teachingStrat, setTeachingStrat] = useState("");
-  const [teachingStratOpen, setTeachingStratOpen] = useState(false);
-
-  const [resource, setResource] = useState("");
-  const [resourceOpen, setResourceOpen] = useState(false);
 
   // Data For Dropdowns:
-  const measures = {
-    "Approaches to Learning": [
-      "ATL-REG 1: Attention Maintenance",
-      "ATL-REG 2: Self-Comforting",
-      "ATL-REG 3: Imitation",
-      "ATL-REG 4: Curiosity and Initiative in Learning",
-      "ATL-REG 5: Self-Control of Feelings and Behavior"
-    ],
-    "Social and Emotional Development": [
-      "SED 1: Identity of Self in Relation to Others",
-      "SED 2: Social and Emotional Understanding",
-      "SED 3: Relationships and Social Interactions with Familiar Adults",
-      "SED 4: Relationships and Social Interactions with Peers",
-      "SED 5: Symbolic and Sociodramatic Play"
-    ],
-    "Language and Literacy": [
-      "LLD1: Understanding of Language (Receptive)",
-      "LLD2: Responsiveness to Language",
-      "LLD 3: Communication and Use of Language (Expressive)",
-      "LLD 4: Reciprocal Communication and Conversation",
-      "LLD 5: Interest in Literacy"
-    ],
-    "Cognition": [
-      "COG 1: Spatial Relationships",
-      "COG 2: Classification",
-      "COG 3: Number Sense of Quantity",
-      "COG 8: Cause and Effect",
-      "COG 9: Inquiry Through Observation and Investigation",
-      "COG 11: Knowledge of the Natural World"
-    ],
-    "Perceptual, Motor, and Physical Development": [
-      "PD-HLTH 1: Perceptual-Motor Skills and Movement Concepts",
-      "PD-HLTH 2: Gross Locomotor Movement Skills",
-      "PD-HLTH 3: Gross Motor Manipulative Skills",
-      "PD-HLTH 4: Fine Motor Skills",
-      "PD-HLTH 5: Safety",
-      "PD-HLTH 6: Personal Care Routines: Hygiene",
-      "PD-HLTH 7: Personal Care Routines: Feeding",
-      "PD-HLTH 8: Personal Care Routines: Dressing"
-    ]
-  };
+  const measures = [
+    // Approaches to Learning
+    { value: 'ATL-REG1', label: 'ATL-REG 1: Attention Maintenance' },
+    { value: 'ATL-REG2', label: 'ATL-REG 2: Self-Comforting' },
+    { value: 'ATL-REG3', label: 'ATL-REG 3: Imitation' },
+    { value: 'ATL-REG4', label: 'ATL-REG 4: Curiosity and Initiative in Learning' },
+    { value: 'ATL-REG5', label: 'ATL-REG 5: Self-Control of Feelings and Behavior' },
+
+    // Social and Emotional Development
+    { value: 'SED1', label: 'SED 1: Identity of Self in Relation to Others' },
+    { value: 'SED2', label: 'SED 2: Social and Emotional Understanding' },
+    { value: 'SED3', label: 'SED 3: Relationships and Social Interactions with Familiar Adults' },
+    { value: 'SED4', label: 'SED 4: Relationships and Social Interactions with Peers' },
+    { value: 'SED5', label: 'SED 5: Symbolic and Sociodramatic Play' },
+
+    // Language and Literacy
+    { value: 'LLD1', label: 'LLD1: Understanding of Language (Receptive)' },
+    { value: 'LLD2', label: 'LLD2: Responsiveness to Language' },
+    { value: 'LLD3', label: 'LLD 3: Communication and Use of Language (Expressive)' },
+    { value: 'LLD4', label: 'LLD 4: Reciprocal Communication and Conversation' },
+    { value: 'LLD5', label: 'LLD 5: Interest in Literacy' },
+
+    // Cognition
+    { value: 'COG1', label: 'COG 1: Spatial Relationships' },
+    { value: 'COG2', label: 'COG 2: Classification' },
+    { value: 'COG3', label: 'COG 3: Number Sense of Quantity' },
+    { value: 'COG8', label: 'COG 8: Cause and Effect' },
+    { value: 'COG9', label: 'COG 9: Inquiry Through Observation and Investigation' },
+    { value: 'COG11', label: 'COG 11: Knowledge of the Natural World' },
+
+    // Perceptual, Motor, and Physical Development
+    { value: 'PD-HLTH1', label: 'PD-HLTH 1: Perceptual-Motor Skills and Movement Concepts' },
+    { value: 'PD-HLTH2', label: 'PD-HLTH 2: Gross Locomotor Movement Skills' },
+    { value: 'PD-HLTH3', label: 'PD-HLTH 3: Gross Motor Manipulative Skills' },
+    { value: 'PD-HLTH4', label: 'PD-HLTH 4: Fine Motor Skills' },
+    { value: 'PD-HLTH5', label: 'PD-HLTH 5: Safety' },
+    { value: 'PD-HLTH6', label: 'PD-HLTH 6: Personal Care Routines: Hygiene' },
+    { value: 'PD-HLTH7', label: 'PD-HLTH 7: Personal Care Routines: Feeding' },
+    { value: 'PD-HLTH8', label: 'PD-HLTH 8: Personal Care Routines: Dressing' },
+  ];
 
   const developmentalLevels = [
-    "Responding Earlier",
-    "Responding Later",
-    "Exploring Earlier",
-    "Exploring Middle",
-    "Exploring Later",
-    "Building Earlier"
+    {value: 'respondingEarlier', label: 'Responding Earlier'},
+    {value: 'respondingLater', label: 'Responding Later'},
+    {value: 'exploringEarlier', label: 'Exploring Earlier'},
+    {value: 'exploringMiddle', label: 'Exploring Middle'},
+    {value: 'exploringLater', label: 'Exploring Later'},
+    {value: 'buildingEarlier', label: 'Building Earlier'},
   ];
 
   const teachingStrats = [
-    "Planned Learning Activity",
-    "Interactions/Teaching Strategies",
-    "Learning Environment and Materials",
-    "Family Engagement",
-    "Professional Growth and Development"
+    { value: 'plannedLearning', label: 'Planned Learning Activity' },
+    { value: 'interactions', label: 'Interactions/Teaching Strategies' },
+    { value: 'learningEnvironment', label: 'Learning Environment and Materials' },
+    { value: 'familyEngagement', label: 'Family Engagement' },
+    { value: 'professionalGrowth', label: 'Professional Growth and Development' },
   ];
 
   const resources = [
-    "Frog Street",
-    "CA Infant/Toddler Frameworks",
-    "ASQ Activities",
-    "ITERS Materials",
-    "HSELOF",
-    "CDC"
-  ]
+    { value: 'frogStreet', label: 'Frog Street' },
+    { value: 'learningGenieFamilyEngagement', label: 'Learning Genie Family Engagement' },
+    { value: 'asqActivities', label: 'ASQ Activities' },
+    { value: 'itersMaterials', label: 'ITERS Materials' },
+  ];
 
   // Functions:
-  const measureDropDown = () => {
-    if (measureOpen){
-      setATLOpen(false);
-      setSEDOpen(false);
-      setLLDOpen(false);
-      setCOGOpen(false);
-      setPHYOpen(false);
-    }
-    setMeasureOpen(!measureOpen);
-  }
 
-  const atlDropDown = () => {
-    setATLOpen(!ATLOpen);
-  }
-
-  const sedDropDown = () => {
-    setSEDOpen(!SEDOpen);
-  }
-
-  const lldDropDown = () => {
-    setLLDOpen(!LLDOpen);
-  }
-
-  const cogDropDown = () => {
-    setCOGOpen(!COGOpen);
-  }
-
-  const phyDropDown = () => {
-    setPHYOpen(!PHYOpen);
-  }
-
-  const developmentalLevelDropDown = () => {
-    setDevelopmentalLevelOpen(!developmentalLevelOpen);
-  }
-
-  const teachingStratDropDown = () => {
-    setTeachingStratOpen(!teachingStratOpen);
-  }
-
-  const resourceDropDown = () => {
-    setResourceOpen(!resourceOpen);
-  }
 
 
   // Grab data from sampleData.csv
@@ -162,78 +107,37 @@ function App() {
       <h1>Early Head Start's "DRDP Tool"</h1>
     </div>
 
+
+
       <div className="dropdowns">
-        <div className="dropdownContainer">
-          <button onClick={measureDropDown} className="dropdown">
-            DRDP Measure: COG3, ATL-REG5 {measureOpen ? "▲" : "▼"}
-          </button>
-          {measureOpen && (
-            <>
-              <button onClick={atlDropDown} className = "measureCategory">
-                Approaches to Learning {ATLOpen ? "▲" : "▼"}
-              </button>
-              {ATLOpen &&
-                measures["Approaches to Learning"].map((m) => (
-                  <button key={m} className = "measureStyle">{m}</button>
-                ))}
-
-              <button onClick={sedDropDown} className = "measureCategory">
-                Social and Emotional Development {SEDOpen ? "▲" : "▼"}
-              </button>
-              {SEDOpen &&
-                measures["Social and Emotional Development"].map((m) => (
-                  <button key={m} className = "measureStyle">{m}</button>
-                ))}
-
-              <button onClick={lldDropDown} className = "measureCategory">
-                Language and Literacy {LLDOpen ? "▲" : "▼"}
-              </button>
-              {LLDOpen &&
-                measures["Language and Literacy"].map((m) => (
-                  <button key={m} className = "measureStyle">{m}</button>
-                ))}
-
-              <button onClick={cogDropDown} className = "measureCategory">
-                Cognition {COGOpen ? "▲" : "▼"}
-              </button>
-              {COGOpen &&
-                measures["Cognition"].map((m) => (
-                <button key={m} className = "measureStyle">{m}</button>
-                ))}
-
-              <button onClick={phyDropDown} className = "measureCategory">
-                Perceptual, Motor, and Physical Development {PHYOpen ? "▲" : "▼"}
-              </button>
-              {PHYOpen &&
-                measures["Perceptual, Motor, and Physical Development"].map((m) => (
-                  <button key={m} className = "measureStyle">{m}</button>
-                ))}
-            </>
-          )}
-        </div>
-
-        <div className="dropdownContainer">
-          <button onClick={developmentalLevelDropDown} className="dropdown">
-            Developmental Level {developmentalLevelOpen ? "▲" : "▼"}
-          </button>
-          {developmentalLevelOpen &&
-            developmentalLevels.map((lvl) => <div key={lvl}>{lvl}</div>)}
-        </div>
-
-        <div className="dropdownContainer">
-          <button onClick={teachingStratDropDown} className="dropdown">
-            Teaching Strategy {teachingStratOpen ? "▲" : "▼"}
-          </button>
-          {teachingStratOpen &&
-            teachingStrats.map((ts) => <div key={ts}>{ts}</div>)}
-        </div>
-
-        <div className="dropdownContainer">
-          <button onClick={resourceDropDown} className="dropdown">
-            Resources {resourceOpen ? "▲" : "▼"}
-          </button>
-          {resourceOpen && resources.map((r) => <div key={r}>{r}</div>)}
-        </div>
+        <Select
+          defaultValue = {selectedMeasure}
+          onChange={(selected) => setSelectedMeasure(selected)}
+          placeholder="Select DRDP Measure"
+          options = {measures}
+          isMulit
+        />
+        <Select
+          defaultValue = {selectedDevelopmentalLevel}
+          onChange={(selected) => setSelectedDevelopmentalLevel(selected)}
+          placeholder="Select Developmental Level"
+          options = {developmentalLevels}
+          isMulit
+        />
+        <Select
+          defaultValue = {selectedTeachingStrats}
+          onChange={(selected) => setSelectedTeachingStrats(selected)}
+          placeholder="Select Teaching Strategy"
+          options = {teachingStrats}
+          isMulit
+        />
+        <Select
+          defaultValue = {selectedResources}
+          onChange={(selected) => setSelectedResources(selected)}
+          placeholder="Select Resources"
+          options = {resources}
+          isMulit
+        />
       </div>
 
       <div className="blob-outer">
