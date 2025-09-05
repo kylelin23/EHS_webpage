464e16d39a37795cd7409dea11ca505bfc010ee9
Kyle Lin <linkyle0924@gmail.com>
2025-09-04 23:47:55 -0700

    Fix Measures

--- src/dataPage.jsx
+++ src/dataPage.jsx
@@ -26,48 +26,7 @@ function DataPage() {
   const [COGOpen, setCOGOpen] = useState(false);
   const [PHYOpen, setPHYOpen] = useState(false);
 
-
-  // Data For Dropdowns:
-  // const measures = [
-  //   // Approaches to Learning
-  //   { value: 'ATL-REG 1: Attention Maintenance', label: 'ATL-REG 1: Attention Maintenance' },
-  //   { value: 'ATL-REG 2: Self-Comforting', label: 'ATL-REG 2: Self-Comforting' },
-  //   { value: 'ATL-REG 3: Imitation', label: 'ATL-REG 3: Imitation' },
-  //   { value: 'ATL-REG 4: Curiosity and Initiative in Learning', label: 'ATL-REG 4: Curiosity and Initiative in Learning' },
-  //   { value: 'ATL-REG 5: Self-Control of Feelings and Behavior', label: 'ATL-REG 5: Self-Control of Feelings and Behavior' },
-
-  //   // Social and Emotional Development
-  //   { value: 'SED 1: Identity of Self in Relation to Others', label: 'SED 1: Identity of Self in Relation to Others' },
-  //   { value: 'SED 2: Social and Emotional Understanding', label: 'SED 2: Social and Emotional Understanding' },
-  //   { value: 'SED 3: Relationships and Social Interactions with Familiar Adults', label: 'SED 3: Relationships and Social Interactions with Familiar Adults' },
-  //   { value: 'SED 4: Relationships and Social Interactions with Peers', label: 'SED 4: Relationships and Social Interactions with Peers' },
-  //   { value: 'SED 5: Symbolic and Sociodramatic Play', label: 'SED 5: Symbolic and Sociodramatic Play' },
-
-  //   // Language and Literacy
-  //   { value: 'LLD 1: Understanding of Language (Receptive)', label: 'LLD1: Understanding of Language (Receptive)' },
-  //   { value: 'LLD 2: Responsiveness to Language', label: 'LLD2: Responsiveness to Language' },
-  //   { value: 'LLD 3: Communication and Use of Language (Expressive)', label: 'LLD 3: Communication and Use of Language (Expressive)' },
-  //   { value: 'LLD 4: Reciprocal Communication and Conversation', label: 'LLD 4: Reciprocal Communication and Conversation' },
-  //   { value: 'LLD 5: Interest in Literacy', label: 'LLD 5: Interest in Literacy' },
-
-  //   // Cognition
-  //   { value: 'COG 1: Spatial Relationships', label: 'COG 1: Spatial Relationships' },
-  //   { value: 'COG 2: Classification', label: 'COG 2: Classification' },
-  //   { value: 'COG 3: Number Sense of Quantity', label: 'COG 3: Number Sense of Quantity' },
-  //   { value: 'COG 8: Cause and Effect', label: 'COG 8: Cause and Effect' },
-  //   { value: 'COG 9: Inquiry Through Observation and Investigation', label: 'COG 9: Inquiry Through Observation and Investigation' },
-  //   { value: 'COG 11: Knowledge of the Natural World', label: 'COG 11: Knowledge of the Natural World' },
-
-  //   // Perceptual, Motor, and Physical Development
-  //   { value: 'PD-HLTH 1: Perceptual-Motor Skills and Movement Concepts', label: 'PD-HLTH 1: Perceptual-Motor Skills and Movement Concepts' },
-  //   { value: 'PD-HLTH 2: Gross Locomotor Movement Skills', label: 'PD-HLTH 2: Gross Locomotor Movement Skills' },
-  //   { value: 'PD-HLTH 3: Gross Motor Manipulative Skills', label: 'PD-HLTH 3: Gross Motor Manipulative Skills' },
-  //   { value: 'PD-HLTH 4: Fine Motor Skills', label: 'PD-HLTH 4: Fine Motor Skills' },
-  //   { value: 'PD-HLTH 5: Safety', label: 'PD-HLTH 5: Safety' },
-  //   { value: 'PD-HLTH 6: Personal Care Routines: Hygiene', label: 'PD-HLTH 6: Personal Care Routines: Hygiene' },
-  //   { value: 'PD-HLTH 7: Personal Care Routines: Feeding', label: 'PD-HLTH 7: Personal Care Routines: Feeding' },
-  //   { value: 'PD-HLTH 8: Personal Care Routines: Dressing', label: 'PD-HLTH 8: Personal Care Routines: Dressing' },
-  // ];
+  // DATA
 
   const measures = {
     "Approaches to Learning": [
@@ -138,33 +97,6 @@ function DataPage() {
     "Attendanceworks.org"
   ]
 
-  // const developmentalLevels = [
-  //   {value: 'Responding Earlier', label: 'Responding Earlier', isImportant: true},
-  //   {value: 'Responding Later', label: 'Responding Later', isImportant: false},
-  //   {value: 'Exploring Earlier', label: 'Exploring Earlier', isImportant: false},
-  //   {value: 'Exploring Middle', label: 'Exploring Middle', isImportant: false},
-  //   {value: 'Exploring Later', label: 'Exploring Later', isImportant: false},
-  //   {value: 'Building Earlier', label: 'Building Earlier', isImportant: false},
-  // ];
-
-  // const teachingStrats = [
-  //   { value: 'Planned Learning Activity', label: 'Planned Learning Activity' },
-  //   { value: 'Interactions/Teaching Strategies', label: 'Interactions/Teaching Strategies' },
-  //   { value: 'Learning Environment and Materials', label: 'Learning Environment and Materials' },
-  //   { value: 'Family Engagement', label: 'Family Engagement' },
-  //   { value: 'Professional Growth and Development', label: 'Professional Growth and Development' },
-  // ];
-
-  // const resources = [
-  //   { value: 'Frog Street', label: 'Frog Street' }, // Add Frog Street Conscious Discipline
-  //   { value: 'HSELOF', label: 'HSELOF' },
-  //   { value: 'ASQ Activities', label: 'ASQ Activities'},
-  //   { value: 'ITERS-3 Materials', label: 'ITERS Materials'},
-  //   { value: 'National Center for Pyramid Model Innovations', label: 'Pyramid Model'},
-  //   { value: 'Center on the Social and Emotional Foundations for Early Learning grantee of HS, CCB', label: 'CSEFEL' },
-  //   { value: 'Attendanceworks.org', label: 'Attendanceworks.org' },
-  // ];
-
   // Functions:
   const navigateBack = () => {
     navigate("/");
@@ -172,34 +104,42 @@ function DataPage() {
 
   const atlDropDown = () => {
     setATLOpen(!ATLOpen);
+    setSEDOpen(false);
+    setLLDOpen(false);
+    setCOGOpen(false);
+    setPHYOpen(false);
   }
 
   const sedDropDown = () => {
     setSEDOpen(!SEDOpen);
+    setATLOpen(false);
+    setLLDOpen(false);
+    setCOGOpen(false);
+    setPHYOpen(false);
   }
 
   const lldDropDown = () => {
     setLLDOpen(!LLDOpen);
+    setATLOpen(false);
+    setSEDOpen(false);
+    setCOGOpen(false);
+    setPHYOpen(false);
   }
 
   const cogDropDown = () => {
     setCOGOpen(!COGOpen);
+    setATLOpen(false);
+    setLLDOpen(false);
+    setSEDOpen(false);
+    setPHYOpen(false);
   }
 
   const phyDropDown = () => {
     setPHYOpen(!PHYOpen);
-  }
-
-  const developmentalLevelDropDown = () => {
-    setDevelopmentalLevelOpen(!developmentalLevelOpen);
-  }
-
-  const teachingStratDropDown = () => {
-    setTeachingStratOpen(!teachingStratOpen);
-  }
-
-  const resourceDropDown = () => {
-    setResourceOpen(!resourceOpen);
+    setATLOpen(false);
+    setLLDOpen(false);
+    setCOGOpen(false);
+    setCOGOpen(false);
   }
 
   const handleDevelopmentalLevelButton = (level) => {
@@ -209,7 +149,6 @@ function DataPage() {
       }
       setSelectedDevelopmentalLevel([...selectedDevelopmentalLevel, level])
     }
-
   }
 
   const handleTeachingStratButton = (teachingStrat) => {
@@ -220,19 +159,6 @@ function DataPage() {
       setSelectedTeachingStrats([...selectedTeachingStrats, teachingStrat])
     }
   }
-
-  // Styling:
-  const customStyles = {
-  option: (provided, state) => ({
-    ...provided,
-    color: state.data.isImportant ? 'red' : 'black', // red if isImportant is true
-    backgroundColor: state.isSelected ? '#ddd' : 'white',
-  }),
-  singleValue: (provided, state) => ({
-    ...provided,
-    color: state.data.isImportant ? 'red' : 'black', // selected value color
-  }),
-};
 
   // Grab data from sampleData.csv
   useEffect(() => {
